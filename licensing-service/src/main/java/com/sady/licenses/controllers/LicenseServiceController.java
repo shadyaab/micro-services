8e55a3417ea7a7d86316c4da12d0b1225de4ad4d
Shadyaab Akhtar <shadyaab.akhtar@lowes.com>
2018-10-25 18:01:30 +0530

    Initial

--- licensing-service/src/main/java/com/sady/licenses/controllers/LicenseServiceController.java
+++ licensing-service/src/main/java/com/sady/licenses/controllers/LicenseServiceController.java
@@ -1,24 +1,18 @@
 package com.sady.licenses.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.sady.licenses.model.License;
-import com.sady.licenses.services.LicenseService;
 
 @RestController
 public class LicenseServiceController {
 
-	@Autowired
-	private LicenseService licenseService;
+	//@Autowired
+	//private LicenseService licenseService;
 	
 	@RequestMapping("/msg")
     public String getLicenses() {
+		
+		
 		return "hello";
     }
 	
