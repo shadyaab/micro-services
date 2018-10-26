fbfd5e7326e1aa433b0877679246560f0e1690b5
Shadyaab Akhtar <shadyaab.akhtar@lowes.com>
2018-10-26 16:24:36 +0530

    Configuration in microservice

--- account/src/main/java/com/sady/account/controllers/AccountController.java
+++ account/src/main/java/com/sady/account/controllers/AccountController.java
@@ -1,13 +1,18 @@
 package com.sady.account.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
 
+	@Autowired
+	private Environment env;
+	
 	@RequestMapping("/msg")
 	public String getMsg(){
-		return "Hello !! Inside account micro-service";
+		return "Hello !! Inside account micro-service : " + env.getProperty("spring.datasource.driver-class-name");
 	}
 }
--- account/src/main/java/com/sady/account/model/Customer.java
+++ account/src/main/java/com/sady/account/model/Customer.java
@@ -1,20 +1,20 @@
 package com.sady.account.model;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Customer {
 
 	@Id
-	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long Id;
 	private String name;
 	private String city;
 	private int age;
-
+	
 	public Long getId() {
 		return Id;
 	}
@@ -46,5 +46,4 @@ public class Customer {
 	public void setAge(int age) {
 		this.age = age;
 	}
-
 }
